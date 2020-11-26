2bc516b3241b60ab988a2ef8e46c9af611a35061
Viranch Mehta <email@viranch.me>
2020-11-25 16:40:53 -0800

    Single network port for Bobarr (web and API)

--- packages/web/utils/api-url.ts
+++ packages/web/utils/api-url.ts
@@ -1,2 +1,2 @@
-const host = typeof window === 'undefined' ? 'api' : window.location.hostname;
-export const apiURL = `http://${host}:4000`;
+export const apiURL = typeof window === 'undefined' ? 'http://api:4000' : window.location.origin + '/api';
+//export const apiURL = `http://${host}:4000`;
